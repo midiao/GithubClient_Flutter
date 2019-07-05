093d542466dafae2d93046a88dbbcb846711bc93
Charles <charles@blurams.com>
2019-07-05 18:26:24 +0800

    <feat>:初始化图片资源。

---
+++ lib/common/constants/assets.dart
@@ -0,0 +1,36 @@
+const String imageHost = 'assets/images/';
+
+/// LoginPage
+const String imageGithubCat = '${imageHost}ic_github_cat_round.png';
+
+/// MainPage
+const String mainNavEventsNormal = '${imageHost}ic_nav_home_normal.png';
+const String mainNavEventsPressed = '${imageHost}ic_nav_home_checked.png';
+const String mainNavReposNormal = '${imageHost}ic_nav_task_normal.png';
+const String mainNavReposPressed = '${imageHost}ic_nav_task_checked.png';
+const String mainNavIssueNormal = '${imageHost}ic_nav_issue_normal.png';
+const String mainNavIssuePressed = '${imageHost}ic_nav_issue_checked.png';
+const String mainNavProfileNormal = '${imageHost}ic_nav_mine_normal.png';
+const String mainNavProfilePressed = '${imageHost}ic_nav_mine_checked.png';
+
+/// eventsPage
+const String eventsForkChecked = '${imageHost}ic_fork_checked.png';
+const String eventsStarChecked = '${imageHost}ic_star_checked.png';
+const String eventsStarUnChecked = '${imageHost}ic_star_checked.png';
+
+/// reposPage
+const String reposFork = '${imageHost}ic_fork_unchecked.png';
+const String reposIssue = '${imageHost}ic_issue_gray.png';
+const String reposStar = '${imageHost}ic_star_unchecked.png';
+
+/// issuesPage
+const String issuesCommentNormal = '${imageHost}ic_issue_comment_normal.png';
+const String issuesCommentNormalDark = '${imageHost}ic_issue_comment_normal_dark.png';
+const String issuesCommentFire = '${imageHost}ic_issue_comment_fire.png';
+const String issuesCommentFireDark = '${imageHost}ic_issue_comment_fire_dark.png';
+const String issuesTag = '${imageHost}ic_issue_tag.png';
+const String issuesTime = '${imageHost}ic_issue_time.png';
+
+/// profilePage
+const String mineShared = '${imageHost}ic_open_in_new_white_24dp.png';
+const String mineLocationIcon = '${imageHost}ic_location_on_gray.png';
---
+++ lib/common/constants/config.dart
@@ -0,0 +1,9 @@
+class Config {
+  static const PAGE_SIZE = 20;
+  static const DEBUG = true;
+  static const USE_NATIVE_WEBVIEW = true;
+
+  static const TOKEN_KEY = "token";
+  static const USER_NAME_KEY = "user-name";
+  static const PW_KEY = "user-pw";
+}
